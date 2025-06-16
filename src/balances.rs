3fe8d7fb769d517cc8ed8000274116f2276f4821
Loz <loz@Lawrences-MacBook-Pro.local>
2025-06-16 16:53:07 +0100

    Build setting and getting balances by who

--- src/balances.rs
+++ src/balances.rs
@@ -9,5 +9,13 @@ impl Pallet {
             balances: BTreeMap::new()
         }
     }
+    //setter function fpor the balance by who defaulting to 0 is not found
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
+        *self.balances.get(who).unwrap_or(&0);
+    }
+    //getter for the balance by who
+    pub fn balance(&self, who: &String) ->  u128 {
+        *self.balances.get(who).unwrap_or(&0)
+    }
 
 }
