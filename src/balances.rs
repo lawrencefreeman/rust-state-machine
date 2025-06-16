ae38d4b3638e4ae387024e9fe63de15fe0b79629
Loz <loz@Lawrences-MacBook-Pro.local>
2025-06-16 17:20:12 +0100

    Fix of set_balance, and addition of tests

--- src/balances.rs
+++ src/balances.rs
@@ -9,13 +9,33 @@ impl Pallet {
             balances: BTreeMap::new()
         }
     }
-    //setter function fpor the balance by who defaulting to 0 is not found
+    //setter function for the balance by who defaulting to 0 is not found
     pub fn set_balance(&mut self, who: &String, amount: u128) {
-        *self.balances.get(who).unwrap_or(&0);
+        self.balances.insert(who.clone(), amount);
     }
     //getter for the balance by who
     pub fn balance(&self, who: &String) ->  u128 {
         *self.balances.get(who).unwrap_or(&0)
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+	#[test]
+	fn init_balances() {
+		/* TODO: Create a mutable variable `balances`, which is a new instance of `Pallet`. */
+        let mut balances = Pallet::new();
+		/* TODO: Assert that the balance of `alice` starts at zero. */
+        //these init of Alice and Bob or whoever work becuase of the defaul &0 return from unwrap_or
+        assert_eq!(balances.balance(&String::from("Alice")), 0);
+		/* TODO: Set the balance of `alice` to 100. */
+        balances.set_balance(&String::from("Alice"), 100);
+		/* TODO: Assert the balance of `alice` is now 100. */
+        assert_eq!(balances.balance(&String::from("Alice")), 100);
+		/* TODO: Assert the balance of `bob` has not changed and is 0. */
+        assert_eq!(balances.balance(&String::from("Bob")), 0);
+	}
 }
