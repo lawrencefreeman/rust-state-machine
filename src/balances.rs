e0bdfefdad0610b814abb48fde6b3962f4ac792c
Loz <loz@Lawrences-MacBook-Pro.local>
2025-06-16 18:09:02 +0100

    added funds transfer mechanism including tests

--- src/balances.rs
+++ src/balances.rs
@@ -18,6 +18,28 @@ impl Pallet {
         *self.balances.get(who).unwrap_or(&0)
     }
 
+    //build the transfer method
+    pub fn transfer(&mut self, from: &String, to: &String, amount: u128) -> Result<(), &'static str> {
+            let from_bal = self.balance(&from);
+            let to_bal = self.balance(&to);
+            //safe math for new "from" bal after amt transf out
+            let new_from_bal = match from_bal.checked_sub(amount) {
+                Some(nb) => nb,
+                None => {
+                        println!("Not enough funds.");
+                        return Err("Not enough funds");
+                }
+            };
+            //no need to handle overflow on add as no amount of DOT would overflow u128
+            let new_to_bal = to_bal.checked_add(amount).unwrap();
+            // Apply the balance updates
+            self.set_balance(from, new_from_bal);
+
+            self.set_balance(to, new_to_bal);
+            
+            Ok(()) //returning the Unit block if all okay.
+        }
+
 }
 
 #[cfg(test)]
@@ -38,4 +60,16 @@ mod tests {
 		/* TODO: Assert the balance of `bob` has not changed and is 0. */
         assert_eq!(balances.balance(&String::from("Bob")), 0);
 	}
+    #[test]
+    fn transfer_amt() {
+        let mut balances = Pallet::new();
+        balances.set_balance(&String::from("Alice"), 100);
+        //check that alice cannot xfer funds she doesnt have
+        assert_eq!(balances.transfer(&String::from("Alice"), &String::from("Bob"), 101), Err("Not enough funds"));
+
+        //check that within balance xfers will work - transfering 99 of 100
+        assert_eq!(balances.transfer(&String::from("Alice"), &String::from("Bob"), 99), Ok(()));
+        assert_eq!(balances.balance(&String::from("Alice")), 1); //100-99=1
+        assert_eq!(balances.balance(&String::from("Bob")), 99); //0+99=1
+    }
 }
