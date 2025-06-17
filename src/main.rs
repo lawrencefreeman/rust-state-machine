adbd3980e146e371c0e6ca411ed28a69c6e13be4
Loz <loz@Lawrences-MacBook-Pro.local>
2025-06-17 08:25:01 +0100

    Initialise Runtime stub-code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 mod balances;
 mod system;
 
+// This is our main Runtime.
+// It accumulates all of the different pallets we want to use.
+pub struct Runtime {
+	system: system::Pallet,
+    balances: balances::Pallet,
+    
+    /* TODO:
+		- Create a field `system` which is of type `system::Pallet`.
+		- Create a field `balances` which is of type `balances::Pallet`.
+	*/
+}
+
+impl Runtime {
+	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
+	fn new() -> Self {
+		/* TODO: Create a new `Runtime` by creating new instances of `system` and `balances`. */
+		Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new(),
+        }
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	println!("Hello, world!");
 }
