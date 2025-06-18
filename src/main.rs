663eb006057dc33e6ee868de3daa194b831978f1
Loz <loz@Lawrences-MacBook-Pro.local>
2025-06-18 12:30:11 +0100

    ADD: built first full block using extrinsics and header with test data

--- src/main.rs
+++ src/main.rs
@@ -105,50 +105,41 @@ fn main() {
     println!("The before state system pallet contents is {:#?}", runtime.system);
     println!("The before state balances pallet contents is {:#?}", runtime.balances);
     
-    let alice = &String::from("Alice");
-    let bob = &String::from("Bob");
-    let charlie = &String::from("Charlie");
+    let alice = String::from("Alice"); //remove reference
+    let bob = String::from("Bob"); //remove reference
+    let charlie = String::from("Charlie"); //remove reference
 
-    runtime.balances.set_balance(alice, 100);
-    runtime.system.inc_block_number();
-    runtime.system.inc_nonce(alice);
+    runtime.balances.set_balance(&alice, 100);
     
-    let _res = runtime.balances.transfer(alice, bob, 30).map_err(|e| eprintln!("{}", e));
-    runtime.system.inc_nonce(alice);
-    
-    let _ = runtime.balances.transfer(alice, charlie, 20);
     println!("The after state system pallet contents is {:#?}", runtime.system);
     println!("The after state balances pallet contents is {:#?}", runtime.balances);
-}
 
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_runtime() {
-        let mut runtime = Runtime::new();
-        let alice = &String::from("Alice");
-        runtime.balances.set_balance(alice, 100);
-        runtime.system.inc_block_number();
-        // TODO: Assert the block number is what we expect
-        assert_eq!(runtime.system.block_number(), 1);
-        // TODO: Increment the nonce of `alice`
-        runtime.system.inc_nonce(alice);
-        //assert the nonce of alice is 1.
-        assert_eq!(runtime.system.get_nonce(alice), 1); //test passes
-        //execute a transfer from alice to bob for 30 tokens.
-        let bob = &String::from("Bob");
-        //using map_err to handle err with anomymous function || https://doc.rust-lang.org/std/result/enum.Result.html#method.map_err
-        let _res = runtime.balances.transfer(alice, bob, 30).map_err(|e| eprintln!("{}", e));
-        assert_eq!(runtime.balances.balance(alice), 70); //test passes
-        //increment the nonce of alice again (to 2)
-        runtime.system.inc_nonce(alice);
-        assert_eq!(runtime.system.get_nonce(alice), 2);
-        //do another bal xfer from alice to charlie
-        let charlie = &String::from("Charlie");
-        runtime.balances.transfer(alice, charlie, 20);
-        assert_eq!(runtime.balances.balance(alice), 50);
-    }
+    /*
+		TODO: Replace the logic above with a new `Block`.
+			- Set the block number to 1 in the `Header`.
+			- Move your existing transactions into extrinsic format, using the
+			  `Extrinsic` and `RuntimeCall`.
+	*/
+    let block_1 = types::Block {
+	header: support::Header { block_number: 1 },
+	extrinsics: vec![
+		support::Extrinsic {
+			caller: alice.clone(),
+			call: RuntimeCall::BalancesTransfer { to: bob, amount: 30 },
+		},
+        support::Extrinsic {
+			caller: alice.clone(),
+			call: RuntimeCall::BalancesTransfer { to: charlie, amount: 20 },
+		},
+	],
+};
+	/*
+		TODO:
+		Use your `runtime` to call the `execute_block` function with your new block.
+		If the `execute_block` function returns an error, you should panic!
+		We `expect` that all the blocks being executed must be valid.
+	*/
+    runtime.execute_block(block_1).expect("invalid block");
+	// Simply print the debug format of our runtime state.
+	println!("{:#?}", runtime);
 }
